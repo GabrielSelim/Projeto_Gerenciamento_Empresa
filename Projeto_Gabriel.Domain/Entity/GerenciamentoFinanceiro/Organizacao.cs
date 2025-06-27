39efc2f2b9079545456b453be1e507fc0fb2b516
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2025-06-27 09:54:27 -0400

    Refactor SQL tables and add Organizacao class
    Removed multiple `INSERT` statements from `books`, `usuarios`, and `pessoas` tables. Updated `CREATE TABLE` statements to eliminate specific columns and constraints in several tables, including `cartas_pokemon`, `log_entries`, and `contatos`. Added a new `role` column to the `usuarios` table. Introduced a new `Organizacao` class in the C# project to manage organizational data with relevant properties and navigation collections.

---
+++ Projeto_Gabriel.Domain/Entity/GerenciamentoFinanceiro/Organizacao.cs
@@ -0,0 +1,30 @@
+﻿using Projeto_Gabriel.Domain.Entity.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_Gabriel.Domain.Entity.GerenciamentoFinanceiro
+{
+    public class Organizacao : BaseEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string NomeFantasia { get; set; }
+
+        [StringLength(100)]
+        public string RazaoSocial { get; set; }
+
+        [StringLength(18)] // Formato CNPJ: 00.000.000/0000-00
+        public string Cnpj { get; set; }
+
+        public string LogoUrl { get; set; }
+
+        public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+
+
+        // Propriedades de Navegação (Relações)
+        public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+        public virtual ICollection<Produto> Produtos { get; set; } = new List<Produto>();
+        public virtual ICollection<Cliente> Clientes { get; set; } = new List<Cliente>();
+        public virtual ICollection<Fornecedor> Fornecedores { get; set; } = new List<Fornecedor>();
+        public virtual ICollection<Transacao> Transacoes { get; set; } = new List<Transacao>();
+    }
+}
